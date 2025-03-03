93bb54e39516768aac0f24354a99b1d86ab83076
julienokumu <codewithjulien@gmail.com>
2025-03-03 22:13:20 +0300

    multi-head attention

--- gpt2.py
+++ gpt2.py
@@ -2,13 +2,13 @@ import torch
 import torch.nn as nn
 from datasets import load_dataset
 
-GPT2_CONFIG_124M = {
+GPT2_124M_CONFIG = {
     "vocab_size": 50257,
     "context_length": 64,
     "emb_dim": 768,
-    "num_heads": 12,
-    "num_layers": 12,
-    "drop_rate": 0.1,
+    "n_heads": 12,
+    "n_layers": 12,
+    "dropout": 0.1,
     "qkv_bias": False
 }
 
@@ -16,40 +16,42 @@ GPT2_CONFIG_124M = {
 class MultiHeadAttention(nn.Module):
     def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
         super().__init__()
-        assert (d_out & num_heads == 0), "d_out must be divisible by num_heads"
+        assert(d_out % num_heads == 0), "d_out must be divisible by num_heads"
         self.d_out = d_out
         self.num_heads = num_heads
-        self.head_dim = d_out // num_heads # size per head
+        self.head_dim = d_out // num_heads # size of each head
         self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.out_proj = nn.Linear(d_out, d_out)
         self.dropout = nn.Dropout(dropout)
-        self.register_buffer("mask", torch.triu(torch.ones(context_length, context_length), diagonal=1)) # casual attention
-
+        # mask to prevent future seeking
+        self.register_buffer("mask", torch.triu(torch.ones(context_length, context_length), diagonal=1))
+    
     def forward(self, x):
-        b, num_tokens, d_in = x.shape
-        queries = self.W_query(x)
+        b, num_tokens, d_in = x.shape()
+        # transfrom input to keys, queries and values
         keys = self.W_key(x)
+        queries = self.W_query(x)
         values = self.W_value(x)
-        # reshape for multiheads
-        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+        # reshape for multiple heads
         keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
         queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
-        # swap with tokens
-        queries = queries.transpose(1, 2)
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+        # swap with token
         keys = keys.transpose(1, 2)
+        queries = queries.transpose(1, 2)
         values = values.transpose(1, 2)
-        # calc attn scores
+        # calculate attn scores
         attn_scores = queries @ keys.transpose(2, 3)
-        # prevent future peeking
+        # get portion of mask matching future tokens, prevents future peeking
         mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
-        # set future tokens to -infinity so they're ignored
+        # set future tokens to -inf so theyre ignored
         attn_scores.masked_fill_(mask_bool, -torch.inf)
-        # normalize scores, stability
+        # normalize scores, for stability
         attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
-        attn_weights = self.dropout(attn_weights)
-        # combine w + values = attn out
+        att_weights = self.dropout(attn_weights)
+        # combine w with v to get attn out
         context_vec = (attn_weights @ values).transpose(1, 2)
         # flatten back to original shape
         context_vec = context_vec.contagious().view(b, num_tokens, self.d_out)
