f5fa46f4b5c775012377df8bf7cecf00d7c30350
julienokumu <codewithjulien@gmail.com>
2025-03-05 16:48:26 +0300

    transformer blocks & full gpt2 model

--- gpt2.py
+++ gpt2.py
@@ -2,12 +2,29 @@ import torch
 import torch.nn as nn
 from datasets import load_dataset
 
+"""
+vocab_size = total number of tokens our model can handle
+context_length = total numner of tokens our model looks at at once
+emb_dim = vector representation of each word
+n_head = number of attention heads
+n_layer = number of transformer layers
+qkv_bias = whether to include a bias in qkv
+drop_rate = rate at which our model ignores some data to avoid overfitting
+tokens = individual words
+logits = prediction scores for which word is likely to come next
+transformer = layer that combines attention and feedforwardnet
+query = questions to ask
+key = what we are looking for
+value = what anser to give
+attn_scores = how much each word cares about other words
+"""
+
 GPT2_124M_CONFIG = {
     "vocab_size": 50257,
     "context_length": 64,
-    "emd_dim": 768,
-    "n_heads": 12,
-    "n_layers": 12,
+    "emb_dim": 768,
+    "n_head": 12,
+    "n_layer": 12,
     "drop_rate": 0.1,
     "qkv_bias": False
 }
@@ -16,87 +33,93 @@ GPT2_124M_CONFIG = {
 class MultiHeadAttention(nn.Module):
     def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
         super().__init__()
-        assert (d_out % num_heads == 0),"d_out must be divisible by num_heads"
+        assert (d_out % num_heads == 0), "d_out must be divisible by num_heads"
         self.d_out = d_out
         self.num_heads = num_heads
-        self.head_dim = d_out // num_heads # size each head chunks(64)
+        self.head_dim = d_out // num_heads # chunk per head
+        # linear layers for the q, k, v
         self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
-        self.out_proj = nn.Linear(d_out, d_out) # organized answer
+        # final layer combining attn outputs
+        self.out_proj = nn.Linear(d_out, d_out)
         self.dropout = nn.Dropout(dropout)
-        # mask to avoid future peeking
-        self.register_buffer("mask", torch.triu(torch.ones(context_length, context_length), diagonal=1))
+        # prevent future peeking
+        self.register_buffer = ("mask", torch.triu(torch.ones(context_length, context_length), diagonal=1))
 
+    # data flow in mah
     def forward(self, x):
-        b, num_tokens, d_in = x.shape # get from input
-        # transform input to
-        keys = self.W_key(x)
+        b, num_tokens, d_in = x.shape
+        # transform input to q, k, v
         queries = self.W_query(x)
+        keys = self.W_key(x)
         values = self.W_value(x)
-        # split into heads
-        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        # reshape for attn heads
         queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
         values = values.view(b, num_tokens, self.num_heads, self.head_dim)
         # swap tokens with
-        keys = keys.transpose(1, 2)
         queries = queries.transpose(1, 2)
+        keys = keys.transpose(1, 2)
         values = values.transpose(1, 2)
-        # attn scores, how each word matters
+        # attn scores
         attn_scores = queries @ keys.transpose(2, 3)
-        # prevent future peeking
+        # mask out future peeking
         mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
-        # set future scores to -inf so theyre ignored
+        # future score to -infinitiy so theyre ignored
         attn_scores.masked_fill_(mask_bool, -torch.inf)
-        # normalize scores into weights(0 - 1)
+        # normalize scores into weight, softmax
         attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
         attn_weights = self.dropout(attn_weights)
-        # combine weights with values = attn output
-        context_vec = context_vec.contagious().view(b, num_tokens, self.d_out)
-        context_vec = self.out_proj(context_vec)
+        # combine weights to values
+        context_vec = (attn_weights @ values).transpose(1, 2)
+        # flatten back to original shape
+        context_vec = context_vec.contaguous().view(b, num_tokens, self.d_out)
+        context_vec = self.out_proj(context_vec) # final linear transformation
         return context_vec # return attn-processed data
     
-
-#  layer normalization
+# layer normalization
 class LayerNorm(nn.Module):
     def __init__(self, emb_dim):
         super().__init__()
         self.eps = 1e-5
-        self.scale = nn.Parameter(torch.one(emb_dim))
-        self.shift = nn.Parameter(torch.zeros(emb_dim))
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.ones(emb_dim))
 
     def forward(self, x):
-        mean = x.mean(dim=-1, keepdim=True) # mean across emb
+        mean = x.mean(dim=-1, keepdim=True) # mean across last emb
         var = x.var(dim=-1, keepdim=True, unbiased=False) # spread of values
         norm_x = (x - mean) / torch.sqrt(var + self.eps) # normalize
-        return self.scale * norm_x + self.shift # scale and shift normalized data
+        return self.scale * norm_x + self.shift # scale and shift the normalized data
     
- # thinking curve   
+# thinking curve
 class GELU(nn.Module):
     def __init__(self):
         super().__init__()
 
     def forward(self, x):
         # mixes linear and non-linear behaviour
-        return 0.5 * x * (1 + torch.tanh(torch.sqrt(torch.tensor(2 / torch.pi)) * (x + 0.044715 * torch.pow(x, 3))))
+        return 0.5 * x * (1 + torch.tanh(torch.sqrt(torch.tensor(2.0 / torch.pi)) * (x + 0.044715 * torch.pow(x, 3))))
     
-# feed forward network, extra thinking
+# feedforward network
 class FeedForward(nn.Module):
     def __init__(self, cfg):
         super().__init__()
+        # sequence of layers
         self.layers = nn.Sequential(
             nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]), # expand input
             GELU(),
-            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]) # shrink input
+            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]), # shrink back
         )
 
     def forward(self, x):
-        return self.layers(x) # return processed data
+        return self.layers(x) # pass input through layers and return result
     
-# transformer block
+# transformer block, attn + feedforward
 class TransformerBlock(nn.Module):
     def __init__(self, cfg):
         super().__init__()
+        # attn with config settings
         self.att = MultiHeadAttention(
             d_in=cfg["emb_dim"],
             d_out=cfg["emb_dim"],
@@ -105,32 +128,33 @@ class TransformerBlock(nn.Module):
             dropout=cfg["drop_rate"],
             qkv_bias=cfg["qkv_bias"]
         )
+        # feedforward net with config settings
         self.ff = FeedForward(cfg)
         self.norm1 = LayerNorm(cfg["emb_dim"])
         self.norm2 = LayerNorm(cfg["emb_dim"])
         self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
 
     def forward(self, x):
-        shortcut = x # save input
+        shortcut = x # save input for residual connection
         x = self.norm1(x)
         x = self.att(x)
         x = self.drop_shortcut(x)
-        x = x + shortcut
-        shortcut = x
+        x = x + shortcut # add original input
+        shorcut = x # save input for next residual connection
         x = self.norm2(x)
         x = self.ff(x)
-        x = self.dropout_shortcut(x)
+        x = self.drop_shortcut(x)
         x = x + shortcut
         return x # return processed data
     
-# full gpt2 model
-class GPT2Model(nn.Module):
+# GPT2 Model
+class GPT2(nn.Module):
     def __init__(self, cfg):
         super().__init__()
+        # token and positional emb layers
         self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
         self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
-        self.drop_emb = nn.Dropout(cfg["drop_rate"])
-        # stack transformer blocks
+        # stack 12 transformer blocks sequentially
         self.trf_blocks = nn.Sequential(
             *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])]
         )
@@ -140,12 +164,12 @@ class GPT2Model(nn.Module):
     def forward(self, in_idx):
         batch_size, seq_len = in_idx.shape
         tok_embeds = self.tok_emb(in_idx) # convert token ids to emb
-        # get pos emb for each position in sequence
+        # positional emb for each position in the sequence
         pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
-        x = tok_embeds + pos_embeds
+        x = pos_embeds + tok_embeds
         x = self.drop_emb(x)
         x = self.trf_blocks(x) # pass through all trf blocks
         x = self.final_norm(x)
-        logits = self.out_head(x) # convert to logists(scores for each token)
+        logits = self.out_head(x) # convert to logits
         return logits # return the predictions
-    
+        
